6e4192f5caca0175dd4036f443153bcca6145288
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2018-10-30 16:21:52 +0100

    fix syntax

--- src/components/BasicAddToHomeScreen.vue
+++ src/components/BasicAddToHomeScreen.vue
@@ -5,14 +5,16 @@
 </template>
 
 
-<script>
-export default {
-  data() {
-    return {
-      defferedPrompt: null,
-    }
-  },
-  mounted() {
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'BasicAddToHomeScreen',
+})
+export default class BasicAddToHomeScreen extends Vue {
+  private deferredPrompt: Event | null = null
+
+  private mounted() {
     console.log('mounted')
     window.addEventListener('beforeinstallprompt', event => {
       console.log('before install')
@@ -21,21 +23,22 @@ export default {
       // Stash the event so it can be triggered later.
       this.deferredPrompt = event
     })
-  },
-  methods: {
-    addToHomeScreen() {
-      this.deferredPrompt.prompt()
-      // Wait for the user to respond to the prompt
-      this.deferredPrompt.userChoice.then(choiceResult => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt')
-        } else {
-          console.log('User dismissed the A2HS prompt')
-        }
-        this.deferredPrompt = null
-      })
-    },
-  },
+  }
+
+  private addToHomeScreen() {
+    // @ts-ignore
+    this.deferredPrompt.prompt()
+    // Wait for the user to respond to the prompt
+    // @ts-ignore
+    this.deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt')
+      } else {
+        console.log('User dismissed the A2HS prompt')
+      }
+      this.deferredPrompt = null
+    })
+  }
 }
 </script>
 
